96cfa2fae2f67b605831f108499b3d1a65183dfc
DESKTOP-FLPOG5V\강성엽 <piz115@naver.com>
2023-04-09 20:04:05 +0900

    Initial commit

---
+++ src/main/java/com/example/dpicaalarmserivce/DpicAAlarmSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dpicaalarmserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpicAAlarmSerivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpicAAlarmSerivceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dpicaalarmserivce/DpicAAlarmSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dpicaalarmserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpicAAlarmSerivceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
